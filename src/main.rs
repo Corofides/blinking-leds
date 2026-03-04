a5fc0c5484f7edfebcb89af27a1f895184bdbd7b
corofides <jamlendrem@gmail.com>
2026-03-04 17:46:11 +0000

    LED blinks once per second;

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,68 @@ use avr_device::atmega328p;
 fn main() -> ! {
     let mut number: i32 = 0;
 
+    let mut is_on = false;
+
     let dp = atmega328p::Peripherals::take().unwrap();
-    
+
+    let tccr: *mut u8 = 0x45 as *mut u8;
+    let timer: *mut u8 = 0x46 as *mut u8;
+
+    let mut last_timer_value: u8 = 0;
+    let mut total_timer_value: u32 = 0;
+
+    let cycles_per_second: u32 =  (16000000.0 / 1024.0) as u32;
+
+
+    unsafe {
+        tccr.write_volatile(0b0000_0101);
+    }
+
     dp.PORTB.ddrb.write(|w| w.pb5().set_bit());
     dp.PORTB.portb.write(|w| w.pb5().set_bit());
 
     loop {
 
+        let mut current_timer = unsafe {
+            timer.read_volatile()
+        };
+
+        let mut delta: u32 = 0;
+
+        if current_timer < last_timer_value {
+            let mut last_timer_value: u32 = last_timer_value as u32;
+            let mut current_timer: u32 = current_timer as u32;
+            
+            current_timer = current_timer + 256;
+
+            delta = current_timer - last_timer_value
+
+        } else {
+            delta = (current_timer - last_timer_value) as u32;
+        }
+
+        //let delta: u32 = delta as u32;
+        total_timer_value = total_timer_value.wrapping_add(delta);
+        last_timer_value = current_timer;
+
+
         number = number.wrapping_add(1);
 
-        dp.PORTB.portb.write(|w| w.pb5().clear_bit());
+        //dp.PORTB.portb.write(|w| w.pb5().clear_bit());
+        
+        if total_timer_value > cycles_per_second {
 
-        if number < (i32::MAX / 2) {
             dp.PORTB.portb.write(|w| w.pb5().set_bit());
+
+            if is_on {
+                dp.PORTB.portb.write(|w| w.pb5().clear_bit());
+            } else {
+                dp.PORTB.portb.write(|w| w.pb5().set_bit());
+            }
+
+            total_timer_value = total_timer_value.saturating_sub(cycles_per_second);
+            is_on = !is_on;
         }
+        
     }
 }
