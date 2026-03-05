433cb486495bee21753e5e56cc393094bc6d233f
corofides <jamlendrem@gmail.com>
2026-03-05 18:03:59 +0000

    Update code so that the flash logic is contained in the interrupt.

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,34 @@ use avr_device::atmega328p::{self, PORTB};
 use avr_device::interrupt::{self, Mutex};
 use core::cell::RefCell;
 
-static TIMER_OVERFLOWED: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
+static OVERFLOW_COUNT: Mutex<RefCell<u8>> = Mutex::new(RefCell::new(0));
 static LED_13: Mutex<RefCell<Option<LED>>> = Mutex::new(RefCell::new(None));
 
 #[avr_device::interrupt(atmega328p)]
 fn TIMER0_OVF() {
-    interrupt::free(|cs| {
-        TIMER_OVERFLOWED.borrow(cs).replace(true);
+    let cycles_per_second: u8 = 61; // 16Mhz / (1024 * 256) Rounded down as it's closer.
+   
+    let overflow_count = interrupt::free(|cs| {
+        let mut overflow_count = OVERFLOW_COUNT.borrow(cs).borrow_mut();
+        *overflow_count = overflow_count.wrapping_add(1);
+        *overflow_count
     });
+
+    if overflow_count >= cycles_per_second {
+
+        interrupt::free(|cs| {
+
+            OVERFLOW_COUNT.borrow(cs).replace(0);
+
+            let mut led_option = LED_13.borrow(cs).borrow_mut();
+
+            if let Some(led) = led_option.as_mut() {
+                led.toggle_led();
+            }
+        });
+
+    }
+    
 }
 
 pub struct LED {
@@ -30,15 +50,7 @@ impl LED {
 #[avr_device::entry]
 fn main() -> ! {
 
-    // Enable interrupts.
-    unsafe {
-        avr_device::interrupt::enable();
-    }
-    let mut overflow_count: u32 = 0;
-
     let dp = atmega328p::Peripherals::take().unwrap();
-
-    let cycles_per_second: u32 =  (16000000.0 / 1024.0) as u32;
 
     // Set up the prescaler.
     dp.TC0.tccr0b.write(|w| {
@@ -61,43 +73,10 @@ fn main() -> ! {
         LED_13.borrow(cs).replace(Some(led13));
     });
 
-    loop {
-
-        // Check if we've overflowed.
-        let timer_overflowed = interrupt::free(|cs| {
-            let mut value = TIMER_OVERFLOWED.borrow(cs).borrow_mut();
-
-            if *value {
-                *value = false;
-                true
-            } else {
-                false
-            }
-
-        });
-
-        // If we've overflowed add 1 to our time count.
-        if timer_overflowed {
-            overflow_count = overflow_count.wrapping_add(1);
-        }
-
-        // Each overflow counts as 256 ticks.
-        if (overflow_count * 256) >= cycles_per_second {
-            overflow_count = 0;
-
-            interrupt::free(|cs| {
-
-                let mut led_option = LED_13.borrow(cs).borrow_mut();
-
-                // .as_mut is important as we don't want to take ownership of the
-                // value, just get a reference to it.
-                if let Some(led) = led_option.as_mut() {
-                    led.toggle_led();
-                }
-
-            });
-
-        }
-                
+    unsafe {
+        avr_device::interrupt::enable();
     }
+
+    // Enable interrupts.
+    loop { /* Do Nothing */ }
 }
